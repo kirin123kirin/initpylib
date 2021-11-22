171fb8d8259589c299cf87b21d2814b561c72df0
kirin123kirin <kirin123kirin@gmail.com>
2021-11-22 15:17:08 +0900

    bugfix

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import sys
-import glob
 from setuptools import setup
 
-glob._ishidden = lambda x: False
+# glob._ishidden = lambda x: False
 
-setup(setup_requires=['pytest-runner>=2.0,<3dev'] if 'pytest' in sys.argv or 'test' in sys.argv else [],
-      data_files=glob.glob('templates_capi/**/*', recursive=True))
+setup(setup_requires=['pytest-runner>=2.0,<3dev'] if 'pytest' in sys.argv or 'test' in sys.argv else [])
